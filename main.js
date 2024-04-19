90a667e9e27e2b5a4dc6bfb7623758749112d8bf
soumengope <soumengop@gmail.com>
2024-04-19 20:00:04 +0530

    updated navigation and UI

--- main.js
+++ main.js
@@ -20,3 +20,26 @@ const observer = new IntersectionObserver(function(entries,observer){
 sec.forEach((e)=>{
     observer.observe(e)
 })
+
+
+const open = document.querySelector('.nav_btn img');
+const close_div = document.querySelector('.nav_btn2');
+const nav_lists = document.querySelector('.nav_lists')
+const nav_main = document.querySelector('.nav_main')
+
+open.onclick = ()=>{
+    nav_lists.style.display = 'block';
+    nav_main.classList.add('nav_stl');
+    document.body.style.overflow = 'hidden';
+    close_div.style.display = 'block'
+    close_div.style.zIndex = '999'
+    open.style.display = 'none'
+}
+close_div.onclick = ()=>{
+    nav_lists.style.display = 'none';
+    nav_main.classList.remove('nav_stl');
+    document.body.style.overflow = 'unset';
+    close_div.style.zIndex = '1'
+    close_div.style.display = 'none'
+    open.style.display = 'block'
+}
